db19b8f772d98b9746ac6b5e6f2d6be4dc73550e
anna.pavanello <annaclaramorettipavanello@gmail.com>
2026-04-17 15:12:00 -0300

    alterações no código

---
+++ src/components/TaksChild.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps(['id', 'descricao', 'status'])
+defineEmits(['editar', 'excluir'])
+</script>
+
+<template>
+<li>
+    {{ descricao }}
+    <a href="#" @click.prevent="$emit('excluir', id)">Excluir</a>
+    <a href="#" @click.prevent="$emit('editar', id)">Editar</a>
+</li>
+<li>
+    {{ status }}
+    <a href="#" @click.prevent="$emit('marcarConcluida', id)">Marcar concluida</a>
+</li>
+</template>
+
+<style scoped>
+
+</style>
